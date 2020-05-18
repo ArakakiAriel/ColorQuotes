e224fc065c380737d5890aab7347cbcd53118034
Ariel Kenji <arakaki.arielkenji@gmail.com>
2020-05-18 14:08:31 +0900

    Initial commit

---
+++ app/src/main/java/com/kenjiman/colorquotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kenjiman.colorquotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
